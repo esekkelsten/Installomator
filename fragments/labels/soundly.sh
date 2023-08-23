453f8795434ffc3a108e1f081edf86fb291a628b
Erlend <erlend.sekkelsten@gmail.com>
2023-08-23 09:06:36 +0200

    Updated version URL
    Replaced URL for appNewVersion.

--- fragments/labels/soundly.sh
+++ fragments/labels/soundly.sh
@@ -3,10 +3,10 @@ soundly)
     type="dmg"
     if [[ $(arch) == "arm64" ]]; then
         downloadURL="https://storage.googleapis.com/soundlyapp/arm/Soundly.dmg"
-        appNewVersion=$(getJSONValue "$(curl -fs https://beta-dot-soundly-server.appspot.com/\?Type\=LatestVersion)" "[0].LatestVersion.MacArm.Version")
+        appNewVersion=$(getJSONValue "$(curl -fs https://soundly.cloud/?Type=LatestVersion)" "[0].LatestVersion.MacArm.Version")
     elif [[ $(arch) == "i386" ]]; then
         downloadURL="https://storage.googleapis.com/soundlyapp/Soundly.dmg"
-        appNewVersion=$(getJSONValue "$(curl -fs https://beta-dot-soundly-server.appspot.com/\?Type\=LatestVersion)" "[0].LatestVersion.MacIntel.Version")
+        appNewVersion=$(getJSONValue "$(curl -fs https://soundly.cloud/?Type=LatestVersion)" "[0].LatestVersion.MacIntel.Version")
     fi
     expectedTeamID="67Y6N7VTDG"
     ;;
